022edd1dddfce1a6917adaef5389eb2e1fc7f28d
danimontoya <daniel.montoya@47deg.com>
2020-04-22 18:25:04 +0200

    Update test cases for coercions

--- src/main/kotlin/arrow/proofs/demo/coercions/coercions.kt
+++ src/main/kotlin/arrow/proofs/demo/coercions/coercions.kt
@@ -4,10 +4,9 @@ import consumer.TwitterHandle
 import consumer.twitterHandle
 import consumer.handle
 
-/**
+/** Still need to check why the first line marker shows up here 📝
  * Some test cases for coercions
  */
-
 // applicableInspection(so it can be changed to be explicit) - highlighting used: Information (so won't be displayed)
 // line marker with proof that validates the coercion
 val implicit: TwitterHandle? = "@danieeehh" // KtProperty
@@ -18,22 +17,21 @@ val implicit2: String = TwitterHandle("@aballano")
 val explicit: TwitterHandle? = "@danieeehh".twitterHandle() // KtDotQualifiedExpression
 val explicit2: String = TwitterHandle("@aballano").handle()
 
-//val twitterHandleValid: TwitterHandle? = "@admin"
-
 fun print(s: String, i: Int, s2: TwitterHandle?) = println(s)
 
 fun implicitExplicitCoercions() =
-    print(TwitterHandle("@aballano").handle(), 1, "@danieeehh") // KtCallElement
+    print(TwitterHandle("@aballano").handle(), 1, "@danieeehh") // KtValueArgument
 
 fun implicitExplicitCoercions2() =
-    print(TwitterHandle("@aballano"), 1, "@danieeehh") // KtCallElement
+    print(TwitterHandle("@aballano"), 1, "@danieeehh") // KtValueArgument
 
 fun implicitExplicitCoercions3() =
-    print(TwitterHandle("@aballano"), 1, "@danieeehh".twitterHandle()) // KtCallElement
+    print(TwitterHandle("@aballano"), 1, "@danieeehh".twitterHandle()) // KtValueArgument
 
 fun implicitExplicitCoercions4() =
-    print(TwitterHandle("@aballano").handle(), 1, "@danieeehh".twitterHandle()) // KtCallElement
+    print(TwitterHandle("@aballano").handle(), 1, "@danieeehh".twitterHandle()) // KtValueArgument
 
-fun print2(s: String, i: Int, s2: TwitterHandle?) = println(s.twitterHandle()?.handle) // KtCallElement
 
-fun print3(s: String, i: Int, s2: TwitterHandle?) = println(TwitterHandle("@aballano")) // KtCallElement
+val implicitExplicit: String? = "@danieeehh".twitterHandle()?.handle() // do we need to handle this case ⁉️⁉️⁉️
+
+val twitterHandleValid: TwitterHandle? = "@admin" // should be highlighted with error because predicate not matching ⁉️⁉️⁉️
