e2235ca17472a8dde6bf8523efc7a2d447ec0d6f
danimontoya <daniel.montoya@47deg.com>
2020-09-22 16:00:49 +0200

    Remove TwitterHandle creation from NonEmptyArray test

--- src/test/kotlin/arrow/proofs/refinement/RefinementTests.kt
+++ src/test/kotlin/arrow/proofs/refinement/RefinementTests.kt
@@ -1,13 +1,10 @@
 package arrow.proofs.refinement
 
-import arrow.Coercion
-import arrow.Refined
 import org.junit.Test
 
 class RefinementTests{
     @Test
     fun `NonEmptyArray should not be empty`() {
         val x: NonEmptyArray = NonEmptyArray(emptyArray())
-        consumer.TwitterHandle("@admin")
     }
 }
